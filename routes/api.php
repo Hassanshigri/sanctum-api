e0855b8686e647728ecc113ecc19e238710f712e
Muhammad Hassan <96192977+Hassanshigri@users.noreply.github.com>
2024-08-16 11:40:58 +0500

    API routes defined

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\API\authController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post('signup',[authController::class,'signup']);
+Route::post('login',[authController::class,'login']);
+
+Route::post('logout',[authController::class,'logout'])->Middleware('auth:sanctum');
